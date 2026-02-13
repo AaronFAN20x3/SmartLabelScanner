d8ed8d691a931aea705b36356648985d82e60dd2
Aaron <xinchuanfan@outlook.com>
2026-02-13 23:28:13 +1000

    Update 1

--- app/src/main/java/com/example/myapplication/ocr/OcrEngine.kt
+++ app/src/main/java/com/example/myapplication/ocr/OcrEngine.kt
@@ -1,33 +1,31 @@
 package com.example.myapplication.ocr
 
-import android.graphics.BitmapFactory
 import com.google.mlkit.vision.common.InputImage
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions
-import java.io.File
 import androidx.camera.core.ImageProxy
-
+@androidx.camera.core.ExperimentalGetImage
 class OcrEngine {
 
     private val recognizer =
         TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
 
-    fun processImage(
-        file: File,
-        onResult: (String) -> Unit
-    ) {
-
-        val bitmap = BitmapFactory.decodeFile(file.absolutePath)
-        val image = InputImage.fromBitmap(bitmap, 0)
-
-        recognizer.process(image)
-            .addOnSuccessListener { visionText ->
-                onResult(visionText.text)
-            }
-            .addOnFailureListener {
-                it.printStackTrace()
-            }
-    }
+    //    fun processImage(
+    //        file: File,
+    //        onResult: (String) -> Unit
+    //    ) {
+    //
+    //        val bitmap = BitmapFactory.decodeFile(file.absolutePath)
+    //        val image = InputImage.fromBitmap(bitmap, 0)
+    //
+    //        recognizer.process(image)
+    //            .addOnSuccessListener { visionText ->
+    //                onResult(visionText.text)
+    //            }
+    //            .addOnFailureListener {
+    //                it.printStackTrace()
+    //            }
+    //    }
 
     fun processImageProxy(
         imageProxy: ImageProxy,
